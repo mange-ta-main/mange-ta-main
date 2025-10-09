828369ec6edac8624acdbace44ec0d007ee165bf
PicoloSaxo <bonnefontfx@gmail.com>
2025-10-09 12:43:58 +0200

    Ajout des données du dataset interactions. Pour les télécharger, hatch run init_data. La méthode de la classe data_loader.py retourne maintenant les deux dataframe chargés depuis les fichiers pickle de interactions et recipes

--- src/mange_ta_main/utils/data_loader.py
+++ src/mange_ta_main/utils/data_loader.py
@@ -3,9 +3,9 @@ from pathlib import Path
 import streamlit as st
 
 DATA_DIR = Path("Data")
-CSV_FILE = DATA_DIR / "RAW_recipes_local.csv"
-PICKLE_FILE = DATA_DIR / "RAW_recipes_local.pkl"
+RECIPES_PICKLE_FILE = DATA_DIR / "RAW_recipes_local.pkl"
+INTERACTIONS_PICKLE_FILE = DATA_DIR / "RAW_interactions_local.pkl"
 
 @st.cache_data
 def load_data():
-    return pd.read_pickle(PICKLE_FILE.resolve())
+    return pd.read_pickle(RECIPES_PICKLE_FILE.resolve()), pd.read_pickle(INTERACTIONS_PICKLE_FILE)
