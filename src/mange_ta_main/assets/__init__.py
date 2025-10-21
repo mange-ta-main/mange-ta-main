1d9bcacad5a4065a17d67d4933adec1976fa562b
zuzmuz <hamadehzaher0@gmail.com>
2025-10-21 11:23:21 +0200

    fixing file name for eating at restaurant

--- src/mange_ta_main/assets/__init__.py
+++ src/mange_ta_main/assets/__init__.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 folder = Path(__file__).parent
 CAMENBEAR = folder / "camembear.png"
-EATING_AT_RESTAURANT = folder / "eating_at_restaurant.png"
+EATING_AT_RESTAURANT = folder / "eating_at_restaurant.jpg"
 JUNK_FOOD = folder / "junk_food.jpg"
