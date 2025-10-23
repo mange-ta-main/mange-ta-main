f4f635b9b5e8c9703bc3ee82b2d628f59bf4b1b3
odelacruz <odelacruz.fierro@gmail.com>
2025-10-23 07:24:48 +0200

    Corrected orthographe error on page top subheader.

--- src/mange_ta_main/assets/__init__.py
+++ src/mange_ta_main/assets/__init__.py
@@ -5,3 +5,4 @@ folder = Path(__file__).parent
 CAMENBEAR = folder / "camembear.png"
 EATING_AT_RESTAURANT = folder / "eating_at_restaurant.jpg"
 JUNK_FOOD = folder / "junk_food.jpg"
+FOOD_DELIVERY = folder / "food_delivery.jpg"
