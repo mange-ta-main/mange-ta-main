f93523118ad9342197fd937845d8ecb8f6822b2f
zuzmuz <hamadehzaher0@gmail.com>
2025-10-23 12:19:58 +0200

    trying out direct deployment

---
+++ scripts/stop_application.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
+echo "----"
+echo "Running stop installation script..." > home/admin/hook.log
+echo "Current date and time: $current_datetime" >> home/admin/hook.log
+echo "----"
