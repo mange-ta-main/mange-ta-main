97bb1a8fa967ebfe755c9eb239e70764d4ba1e03
zuzmuz <hamadehzaher0@gmail.com>
2025-10-27 23:04:25 +0100

    trying pushing file

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -6,4 +6,12 @@ echo "Running start installation script..." >> /home/admin/hook.log
 echo "Current date and time: $current_datetime" >> /home/admin/hook.log
 echo "----"
 
+# Copy service file
+sudo cp "$APP_LOCATION/script/mange-ta-main.service" /etc/systemd/system/mange-ta-main.service
+
+# Reload systemd
+sudo systemctl enable mange-ta-main.service >> /home/admin/hook.log
+
+sudo systemctl daemon-reload >> /home/admin/hook.log
+
 sudo systemctl restart mange-ta-main.service
