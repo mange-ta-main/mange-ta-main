0578760d55d82729c73870157c6a5c0c4cee5690
zuzmuz <hamadehzaher0@gmail.com>
2025-10-27 21:56:40 +0100

    adding scripts to deploy

--- scripts/before_install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
-echo "----"
-echo "Running pre-installation script..." > home/admin/hook.log
-echo "Current date and time: $current_datetime" >> home/admin/hook.log
-echo "----"
--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -2,6 +2,6 @@
 
 current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
 echo "----"
-echo "Running stop installation script..." > home/admin/hook.log
-echo "Current date and time: $current_datetime" >> home/admin/hook.log
+echo "Running stop installation script..." > /home/admin/hook.log
+echo "Current date and time: $current_datetime" >> /home/admin/hook.log
 echo "----"
