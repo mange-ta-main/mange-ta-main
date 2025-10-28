5416bdf2ea7151822e99fa4ea3241ac2150f39b7
odelacruz <odelacruz.fierro@gmail.com>
2025-10-28 15:41:35 +0100

    ODZ: Cleaned page Data.

--- src/mange_ta_main/utils/sidebar.py
+++ src/mange_ta_main/utils/sidebar.py
@@ -1,15 +1,23 @@
 import streamlit as st
 import os
-def kaggle_link():
-    
+
+
+def kaggle_link():    
     st.sidebar.markdown(
-        """
+        """       
         <div align="center">
+        
+        <p><b>
+        <big><big>
+            Data set powered by
+        </big></big>
+        </b></p>
+        
         <a href="https://www.kaggle.com/datasets/shuyangli94/food-com-recipes-and-user-interactions/data" target="_blank">
-            <img src="https://www.kaggle.com/static/images/logos/kaggle-logo-transparent.svg" width="100">
+            <img src="https://www.kaggle.com/static/images/logos/kaggle-logo-transparent.svg" width="150">
         </a>
+        
         </div>
         """,
         unsafe_allow_html=True
-
     )
